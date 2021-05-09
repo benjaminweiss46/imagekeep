93208693b7c2ddd681bdcbbbe23874e2625c07c7
Benjamin Weiss <benjamin.weiss46@gmail.com>
2021-05-08 22:10:32 -0400

    added some styles for the sign in

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -26,51 +26,51 @@ const SignIn = () => {
     };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        {error !== null && <div className = "py-4 bg-red-600 w-full text-white text-center mb-3">{error}</div>}
-        <form className="">
-          <label htmlFor="userEmail" className="block">
+    <div className="sign-in-holder">
+      <h1 className="sign-in-header">ImageKeep</h1>
+      <div className="sign-in">
+        {error !== null && <div className = "error-message">{error}</div>}
+        <form className="sign-in-info">
+          <h3 htmlFor="userEmail" className="email">
             Email:
-          </label>
+          </h3>
           <input
             type="email"
-            className="my-1 p-1 w-full"
+            className="email-input"
             name="userEmail"
             value = {email}
-            placeholder="E.g: faruq123@gmail.com"
+            placeholder="ex. joe.shlomo@gmail.com"
             id="userEmail"
             onChange = {(event) => onChangeHandler(event)}
           />
-          <label htmlFor="userPassword" className="block">
+          <h3 htmlFor="userPassword" className="password">
             Password:
-          </label>
+          </h3>
           <input
             type="password"
-            className="mt-1 mb-3 p-1 w-full"
+            className="password-input"
             name="userPassword"
             value = {password}
             placeholder="Your Password"
             id="userPassword"
             onChange = {(event) => onChangeHandler(event)}
           />
-          <button className="bg-green-400 hover:bg-green-500 w-full py-2 text-white" onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+          <button className="sign-in-button" onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
             Sign in
           </button>
         </form>
-        <p className="text-center my-3">or</p>
+        <p className="or">or</p>
         <button
-          className="bg-red-500 hover:bg-red-600 w-full py-2 text-white">
+          className="sign-in-google-button">
           Sign in with Google
         </button>
-        <p className="text-center my-3">
+        <p className="create-account">
           Don't have an account?{" "}
-          <Link to="signUp" className="text-blue-500 hover:text-blue-600">
+          <Link to="signUp" className="sign-up-page-link">
             Sign up here
           </Link>{" "}
           <br />{" "}
-          <Link to = "passwordReset" className="text-blue-500 hover:text-blue-600">
+          <Link to = "passwordReset" className="reset-password-link">
             Forgot Password?
           </Link>
         </p>
